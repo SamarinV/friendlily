3c93d432a4686ce20fa167fbd194a96c83538558
VladimirS <v.a.samarin@yandex.ru>
2024-04-10 15:46:32 +0300

    router

--- src/index.tsx
+++ src/index.tsx
@@ -8,7 +8,7 @@ import { BrowserRouter } from "react-router-dom"
 
 ReactDOM.render(
   <Provider store={store}>
-    <BrowserRouter basename="social-network">
+    <BrowserRouter basename="/">
       <App />
     </BrowserRouter>
   </Provider>,

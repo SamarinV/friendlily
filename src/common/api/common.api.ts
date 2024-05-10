89f544366608b1bd9e6feca889f8055d453ab87e
VladimirS <v.a.samarin@yandex.ru>
2024-05-10 15:54:11 +0300

    add api key

--- src/common/api/common.api.ts
+++ src/common/api/common.api.ts
@@ -2,6 +2,9 @@ import axios from "axios"
 
 export const instance = axios.create({
   baseURL: "https://social-network.samuraijs.com/api/1.0/",
+  headers: {
+    "API-KEY": "abeb18c3-0a44-443c-b32c-87b09fba1678",
+  },
 })
 
 instance.interceptors.request.use(function (config) {

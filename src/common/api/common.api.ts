c797af2c82a702f9d71dde7154b4d541822a948b
VladimirS <v.a.samarin@yandex.ru>
2024-05-10 14:19:20 +0300

    delete authorisation in headers

--- src/common/api/common.api.ts
+++ src/common/api/common.api.ts
@@ -5,6 +5,5 @@ export const instance = axios.create({
   withCredentials: true,
   headers: {
     "API-KEY": "abeb18c3-0a44-443c-b32c-87b09fba1678",
-    Authorization: "Bearer 84a10472-b3e0-4db7-bf79-a6c641f45ea1",
   },
 })

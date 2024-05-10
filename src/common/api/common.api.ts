25b45e2684e668cf1ca7665ce3a9b9c6291c22d2
VladimirS <v.a.samarin@yandex.ru>
2024-05-10 14:01:37 +0300

    add Bearer

--- src/common/api/common.api.ts
+++ src/common/api/common.api.ts
@@ -5,5 +5,6 @@ export const instance = axios.create({
   withCredentials: true,
   headers: {
     "API-KEY": "694190b5-cb17-4c18-9737-7ae3ad072ed8",
+    Authorization: "Bearer 84a10472-b3e0-4db7-bf79-a6c641f45ea1",
   },
 })

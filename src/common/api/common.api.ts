24601b4c332fa25db99af9631be27ca94e612021
VladimirS <v.a.samarin@yandex.ru>
2024-05-10 15:43:13 +0300

    add interceptor

--- src/common/api/common.api.ts
+++ src/common/api/common.api.ts
@@ -2,8 +2,12 @@ import axios from "axios"
 
 export const instance = axios.create({
   baseURL: "https://social-network.samuraijs.com/api/1.0/",
-  withCredentials: true,
-  headers: {
-    "API-KEY": "abeb18c3-0a44-443c-b32c-87b09fba1678",
-  },
+})
+
+instance.interceptors.request.use(function (config) {
+  if (config.headers) {
+    config.headers["Authorization"] = "Bearer " + localStorage.getItem("sn-token")
+  }
+
+  return config
 })

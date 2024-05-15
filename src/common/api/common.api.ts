d1ec8f77125c357ed871f25118dfd0cc5c0c4f94
VladimirS <v.a.samarin@yandex.ru>
2024-05-15 11:50:30 +0300

    remove widthCredentials

--- src/common/api/common.api.ts
+++ src/common/api/common.api.ts
@@ -2,7 +2,6 @@ import axios from "axios"
 
 export const instance = axios.create({
   baseURL: "https://social-network.samuraijs.com/api/1.0/",
-  withCredentials: true,
   headers: {
     "API-KEY": "045ea086-f26a-486a-bbf3-0e3b3c7753e8",
   },
